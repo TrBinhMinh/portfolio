533a5410dd32ff387d0a64caf3ff687c6014d599
Minh <bminh9900@gmail.com>
2022-11-24 20:14:22 +0700

    update about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,25 @@
 const AboutMe = () => {
   return (
-    <section>
+    <article>
       <h2>About Me</h2>
-      <div></div>
-    </section>
+      <section>
+        <h3>Personal details</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+          reiciendis quis a impedit aliquam ipsam eveniet quae at culpa
+          assumenda eum, blanditiis deserunt dolores! Exercitationem laboriosam
+          iure perferendis mollitia blanditiis. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Voluptates reiciendis quis a impedit
+          aliquam ipsam eveniet quae at culpa assumenda eum, blanditiis deserunt
+          dolores! Exercitationem laboriosam iure perferendis mollitia
+          blanditiis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptates reiciendis quis a impedit aliquam ipsam eveniet quae at
+          culpa assumenda eum, blanditiis deserunt dolores! Exercitationem
+          laboriosam iure perferendis mollitia blanditiis.
+        </p>
+      </section>
+      
+    </article>
   );
 };
 
