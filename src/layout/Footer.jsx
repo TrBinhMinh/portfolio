ff5719e55214b7ce117e14f0baa960c38178a6ea
LDAP\tbminh <tbminh@cmcglobal.vn>
2022-12-14 10:19:37 +0700

    update footer UI

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -3,10 +3,11 @@ import classes from "./Footer.module.scss";
 const Footer = () => {
   return (
     <footer className={classes.container}>
-      <div>
-        <p>Email address</p>
-        <p>Social media</p>
-      </div>
+      <address>
+        <a href="mailto:bminh9900@gmail.com">bminh9900@gmail.com</a>
+        <a href="tel:+84338824186">(+84) 338-824-186</a>
+        <a href="#">Social media</a>
+      </address>
       <div>Address</div>
       <div>Address</div>
     </footer>
