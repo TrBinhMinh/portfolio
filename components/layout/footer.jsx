68c678e7f9821d9d2dea79edd6eddaa0c125aa43
TrBinhMinh <bminh9900@gmail.com>
2022-12-25 23:02:32 +0700

    update footer

--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -1,14 +1,27 @@
-import classes from "./Footer.module.scss";
+import Link from "next/link";
+import classes from "./footer.module.scss";
 
 const Footer = () => {
   return (
     <footer className={classes.container}>
+      <div>
+        <Link className={classes.logo} href="/">
+          &lt;Minh.dev&gt;
+        </Link>
+        <div>
+          <span>Social media:</span> <a href="#">Social media</a>
+        </div>
+      </div>
       <address>
-        <a href="mailto:bminh9900@gmail.com">bminh9900@gmail.com</a>
-        <a href="tel:+84338824186">(+84) 338-824-186</a>
-        <a href="#">Social media</a>
+        <div>
+          <span>Email:</span>{" "}
+          <a href="mailto:bminh9900@gmail.com">bminh9900@gmail.com</a>
+        </div>
+        <div>
+          <span>Phone number:</span>{" "}
+          <a href="tel:+84338824186">(+84) 338-824-186</a>
+        </div>
       </address>
-      <div>Address</div>
       <div>Address</div>
     </footer>
   );
