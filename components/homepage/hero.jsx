0716eb0bcc451248baa32952127b8631fc4f8f4a
LDAP\tbminh <tbminh@cmcglobal.vn>
2022-12-29 16:43:12 +0700

    update homepage background image size

--- components/homepage/hero.jsx
+++ components/homepage/hero.jsx
@@ -1,9 +1,22 @@
+import Image from "next/image";
+import bgImg from "../../public/images/hero-image.jpg";
+
 import classes from "./hero.module.scss";
 
 const Hero = () => {
   return (
     <section className={classes.container}>
       <div className={classes["container--heading-box"]}>
+        <Image
+          src={bgImg}
+          fill
+          priority
+          alt="A screen of website/application development code."
+          placeholder="blur"
+        />
+        <div
+          className={classes["container--heading-box--backdrop-filter"]}
+        ></div>
         <h1 className={classes["container--heading-box--heading"]}>
           Make your website or application runs smoothly!
         </h1>
